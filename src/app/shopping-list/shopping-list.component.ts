bc290610bb1c06dcd81518427f7f32ed3096523d
azohar@cambium.co.il <azohar@cambium.co.il>
2021-02-08 16:19:37 +0200

    add recipe

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Ingredients } from './../shared/ingredient.model';
+import { Component , OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingredients[] = [
+      new Ingredients('Apples',5),
+      new Ingredients('Tomatoes',10),
+  ];
+
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
